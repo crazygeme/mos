f3f0970bfbc4c6087971be9578978ee882ed5339
Zheng, Ender <crazyender@gmail.com>
2026-04-10 15:53:03 +0800

    support 256 color

--- tools/ssh_client.sh
+++ tools/ssh_client.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -oKexAlgorithms=+diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-rsa -oPubkeyAcceptedKeyTypes=+ssh-rsa -oCiphers=+aes128-cbc,3des-cbc ezheng@10.0.5.18
+TERM=xterm ssh -oKexAlgorithms=+diffie-hellman-group-exchange-sha1,diffie-hellman-group1-sha1 -oHostKeyAlgorithms=+ssh-rsa -oPubkeyAcceptedKeyTypes=+ssh-rsa -oCiphers=+aes128-cbc,3des-cbc ezheng@10.0.5.18
